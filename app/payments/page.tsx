d8eee8e4bea5424e1cdeaf0cc130cd002dc3c321
Sapna Jha <sapnajha1919@gmail.com>
2023-11-22 22:47:44 +0530

    Add payment page

---
+++ app/payments/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Payments from "@/components/Payments/Payments";
+
+const PaymentsScreen: React.FC = () => {
+  return <Payments/>;
+};
+
+export default PaymentsScreen;
