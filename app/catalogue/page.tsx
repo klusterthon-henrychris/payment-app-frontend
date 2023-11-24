9c662e93cf0ee9c07f56286803ff5a7e9b170ad7
Sapna Jha <sapnajha1919@gmail.com>
2023-11-24 15:47:29 +0530

    Add catalgoue page

---
+++ app/catalogue/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Catalogue from "@/components/Catalogue/Catalogue";
+
+const CatalogueScreen: React.FC = () => {
+  return <Catalogue/>
+};
+
+export default CatalogueScreen;
